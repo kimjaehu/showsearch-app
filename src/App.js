83940de5e2d34449872c4558ae2843b63504d82a
Jay Kim <kimjaehu@gmail.com>
2019-08-20 13:29:04 -0400

    added search form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+
 import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/home/Landing';
+
+import store from './store';
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <Navbar />
+      <Landing />
       <Footer />
-    </div>
+    </Provider>
   );
 }
 
