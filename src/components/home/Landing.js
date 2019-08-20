cc6fdb80852becbf6ff0aafe0adf0796f2a0a633
Jay Kim <kimjaehu@gmail.com>
2019-08-20 14:34:24 -0400

    prepares for adding loading spinner

--- src/components/home/Landing.js
+++ src/components/home/Landing.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import SearchForm from './SearchForm';
+import Spinner from '../layout/spinner'
 
 export class Landing extends Component {
   render() {
+    
     return (
       <div className='container'>
         <SearchForm />
